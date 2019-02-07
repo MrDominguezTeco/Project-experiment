aef8b2835255cbd45332aecc038eeb46b6be757d
MrDominguezTeco <43441682+MrDominguezTeco@users.noreply.github.com>
2019-02-07 08:31:15 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,101 @@
+
+  $("#up").click(function(){
+  $("#ship4").attr("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMPJ05DC0hbmwHeDu7wGU7oL1pVP6S0GgkJgMiMRa7x7sKjcXzLg");
+  $("#ship7").attr("src", "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMUAAAEACAMAAAA0tEJxAAAAA1BMVEX///+nxBvIAAAASElEQVR4nO3BMQEAAADCoPVPbQwfoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA+BsYAAAF7hZJ0AAAAAElFTkSuQmCC");
+  $("#up").hide();
+  $("#down").hide();
+  $("#ex").hide();
+ 
+  $("#ex2").show();
+
+  
+  
+ });
+
+
+  
+$("#row1").dblclick(function(){
+  $("#ship1").attr("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMPJ05DC0hbmwHeDu7wGU7oL1pVP6S0GgkJgMiMRa7x7sKjcXzLg");
+  $("#ship7").attr("src", "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMUAAAEACAMAAAA0tEJxAAAAA1BMVEX///+nxBvIAAAASElEQVR4nO3BMQEAAADCoPVPbQwfoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA+BsYAAAF7hZJ0AAAAAElFTkSuQmCC");
+   $("#ship4").attr("src", "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMUAAAEACAMAAAA0tEJxAAAAA1BMVEX///+nxBvIAAAASElEQVR4nO3BMQEAAADCoPVPbQwfoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA+BsYAAAF7hZJ0AAAAAElFTkSuQmCC");
+    $("#up").hide();
+  $("#down").hide();
+  $("#ex").hide();
+
+  $("#ex3").show();
+
+   
+});
+$("#down").click(function(){
+  $("#ship10").attr("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMPJ05DC0hbmwHeDu7wGU7oL1pVP6S0GgkJgMiMRa7x7sKjcXzLg");
+  $("#ship7").attr("src", "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMUAAAEACAMAAAA0tEJxAAAAA1BMVEX///+nxBvIAAAASElEQVR4nO3BMQEAAADCoPVPbQwfoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA+BsYAAAF7hZJ0AAAAAElFTkSuQmCC");
+    $("#up").hide();
+  $("#down").hide();
+  $("#ex").hide();
+
+  $("#ex4").show();
+
+});
+
+$("#row5").dblclick(function(){
+  $("#ship13").attr("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMPJ05DC0hbmwHeDu7wGU7oL1pVP6S0GgkJgMiMRa7x7sKjcXzLg");
+  $("#ship7").attr("src", "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMUAAAEACAMAAAA0tEJxAAAAA1BMVEX///+nxBvIAAAASElEQVR4nO3BMQEAAADCoPVPbQwfoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA+BsYAAAF7hZJ0AAAAAElFTkSuQmCC");
+   $("#ship10").attr("src", "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMUAAAEACAMAAAA0tEJxAAAAA1BMVEX///+nxBvIAAAASElEQVR4nO3BMQEAAADCoPVPbQwfoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA+BsYAAAF7hZJ0AAAAAElFTkSuQmCC");
+     $("#up").hide();
+  $("#down").hide();
+  $("#ex").hide();
+
+  $("#ex5").show();
+
+});
+$("#ex").click(function(){
+  $("#ship8").attr("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMPJ05DC0hbmwHeDu7wGU7oL1pVP6S0GgkJgMiMRa7x7sKjcXzLg");
+  $("#ship8").fadeOut();
+  $("#ship9").attr("src", "https://vignette.wikia.nocookie.net/unanything/images/2/28/Saturn.png/revision/latest/scale-to-width-down/366?cb=20180912181854");
+    $("#ship7").attr("src", "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMUAAAEACAMAAAA0tEJxAAAAA1BMVEX///+nxBvIAAAASElEQVR4nO3BMQEAAADCoPVPbQwfoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA+BsYAAAF7hZJ0AAAAAElFTkSuQmCC");
+    $("#row3").css("background-color","white");
+    $("#ex").hide();
+    $("#up").hide();
+  $("#down").hide();
+    $("#text").text("You found Saturn.In Saturn we can't get sufficient amount of sunlight, so it is very cold(approx. - 288 ° F).And it is a gas gaint containing hydrogen and helium mainly also not having a solid surface.The wind speed is very high (approx. 1100 miles/hr). So Saturn is not a good substitue for Earth");
+  }
+  
+);
+$("#ex2").click(function(){
+  $("#ship5").attr("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMPJ05DC0hbmwHeDu7wGU7oL1pVP6S0GgkJgMiMRa7x7sKjcXzLg");
+  $("#ship5").fadeOut();
+  $("#ship6").attr("src", "https://previews.123rf.com/images/2dmolier/2dmolier1803/2dmolier180300706/97191319-jupiter-planet-on-white-background-3d-illustration.jpg");
+    $("#ship4").attr("src", "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMUAAAEACAMAAAA0tEJxAAAAA1BMVEX///+nxBvIAAAASElEQVR4nO3BMQEAAADCoPVPbQwfoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA+BsYAAAF7hZJ0AAAAAElFTkSuQmCC");
+    $("#row2").css("background-color","white");
+    $("#ex2").hide();
+    $("#text").text("You found Jupiter. Jupiter is a gas giant, which means it probably does not have a solid surface, and the gas it is made up of would be toxic for us. It is also very far from the sun (sunlight can take over an hour to get there) which means that is it very cold. Finally, the mass of Jupiter means that its gravitational pull would be too strong. So Jupiter is not a good substitute for Earth.");
+});
+$("#ex3").click(function(){
+  $("#ship2").attr("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMPJ05DC0hbmwHeDu7wGU7oL1pVP6S0GgkJgMiMRa7x7sKjcXzLg");
+  $("#ship2").fadeOut();
+  $("#ship3").attr("src", "https://previews.123rf.com/images/alexokokok/alexokokok1306/alexokokok130600017/20551187-realistic-planet-mercury-isolated-on-white-background-.jpg");
+    $("#ship1").attr("src", "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMUAAAEACAMAAAA0tEJxAAAAA1BMVEX///+nxBvIAAAASElEQVR4nO3BMQEAAADCoPVPbQwfoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA+BsYAAAF7hZJ0AAAAAElFTkSuQmCC");
+    $("#row1").css("background-color","white");
+    $("#ex3").hide();
+    $("#text").text("You found Mercury. Mercury is a barren planet with little atmosphere. In many regards it is similar to our own moon. However, it is also very close to the Sun, so humans will have to set up bases in craters or near the Poles to avoid being blinded. It is extremely difficult, maybe even impossible, to terraform Mercury, considering it would involve creating an atmosphere on a planet with an almost non-existent one due to solar wind bombarding the planet.So Mercury would not be a good substitue for Earth.");
+});
+$("#ex4").click(function(){
+  $("#ship11").attr("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMPJ05DC0hbmwHeDu7wGU7oL1pVP6S0GgkJgMiMRa7x7sKjcXzLg");
+  $("#ship11").fadeOut();
+  $("#ship12").attr("src", "https://previews.123rf.com/images/blueringmedia/blueringmedia1310/blueringmedia131000166/23261263-illustration-of-planet-neptune-on-a-white-background.jpg");
+    $("#ship10").attr("src", "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMUAAAEACAMAAAA0tEJxAAAAA1BMVEX///+nxBvIAAAASElEQVR4nO3BMQEAAADCoPVPbQwfoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA+BsYAAAF7hZJ0AAAAAElFTkSuQmCC");
+    $("#row4").css("background-color","white");
+    $("#ex4").hide();
+    $("#text").text("You found Neptune. The winds blow at 700 miles an hour and it’s around 300 degrees below zero. There’s also no oxygen to breathe. You would be frozen into a Popsicle and then blown across the planet. Also the gravitational pull would literally kill us. So Neptune is not a good substitute for Earth.");
+});
+$("#ex5").click(function(){
+  $("#ship14").attr("src", "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSMPJ05DC0hbmwHeDu7wGU7oL1pVP6S0GgkJgMiMRa7x7sKjcXzLg");
+  $("#ship14").fadeOut();
+  $("#ship15").attr("src", "https://previews.123rf.com/images/2dmolier/2dmolier1803/2dmolier180300697/97191272-mars-planet-on-white-background-3d-illustration.jpg");
+    $("#ship13").attr("src", "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAMUAAAEACAMAAAA0tEJxAAAAA1BMVEX///+nxBvIAAAASElEQVR4nO3BMQEAAADCoPVPbQwfoAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA+BsYAAAF7hZJ0AAAAAElFTkSuQmCC");
+    $("#row5").css("background-color","white");
+    $("#ex5").hide();
+    $("#text").text("You have found Mars. Like Earth, Mars has seasons, polar ice caps, volcanoes, canyons, and weather. It has a very thin atmosphere made of carbon dioxide, nitrogen, and argon.There are signs of ancient floods on Mars, but now water mostly exists in icy dirt and thin clouds. On some Martian hillsides, there is evidence of liquid salty water in the ground. Currently scientists are trying to find a way to terraform Mars being that it would be the best substitue for Earth at the moment.Congrats on finding Mars🏆🏆🏆");
+});
+
+
